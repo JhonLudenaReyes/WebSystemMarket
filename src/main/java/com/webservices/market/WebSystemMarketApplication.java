20c3ca03972efb1a1ebdaa8b7e73a49b34bc3606
JhonLudenaReyes <ludena.jhon.j.6392@outlook.com>
2021-06-04 22:43:41 -0500

    Inicio de proyecto

---
+++ src/main/java/com/webservices/market/WebSystemMarketApplication.java
@@ -0,0 +1,13 @@
+package com.webservices.market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebSystemMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebSystemMarketApplication.class, args);
+	}
+
+}
