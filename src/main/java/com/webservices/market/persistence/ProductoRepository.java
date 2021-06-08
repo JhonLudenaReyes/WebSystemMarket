c304ffe8ddaf7e42aa48ec896edc9cb56b160892
JhonLudenaReyes <ludena.jhon.j.6392@outlook.com>
2021-06-08 14:20:12 -0500

    Producto Repository

--- src/main/java/com/webservices/market/persistence/ProductoRepository.java
+++ src/main/java/com/webservices/market/persistence/ProductoRepository.java
@@ -1,10 +1,14 @@
 package com.webservices.market.persistence;
 
 import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Repository;
 
 import com.webservices.market.persistence.crud.ProductoCrudRepository;
 import com.webservices.market.persistence.entity.Producto;
 
+@Repository
 public class ProductoRepository {
 	
 	private ProductoCrudRepository productoCrudRepository;
@@ -15,6 +19,22 @@ public class ProductoRepository {
 	
 	public List<Producto> getByCategoria(int idCategoria){
 		return productoCrudRepository.findByIdCategoriaOrderByNombreAsc(idCategoria);
+	}
+	
+	public Optional<List<Producto>> getEscasos(int cantidad){
+		return productoCrudRepository.findByCantidadStockLessThanAndEstado(cantidad, true);
+	}
+	
+	public Optional<Producto> getProducto(int idProducto){
+		return productoCrudRepository.findById(idProducto);
+	}
+	
+	public Producto save(Producto producto) {
+		return productoCrudRepository.save(producto);
+	}
+	
+	public void delete(int idProducto) {
+		productoCrudRepository.deleteById(idProducto);
 	}
 
 }
