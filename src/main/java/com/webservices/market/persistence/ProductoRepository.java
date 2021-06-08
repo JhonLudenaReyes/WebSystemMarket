f0ae6697f9532c509862129ee19075ef030422e0
JhonLudenaReyes <ludena.jhon.j.6392@outlook.com>
2021-06-08 12:38:07 -0500

    Query Methods

--- src/main/java/com/webservices/market/persistence/ProductoRepository.java
+++ src/main/java/com/webservices/market/persistence/ProductoRepository.java
@@ -12,5 +12,9 @@ public class ProductoRepository {
 	public List<Producto> getAll(){
 		return (List<Producto>) productoCrudRepository.findAll();
 	}
+	
+	public List<Producto> getByCategoria(int idCategoria){
+		return productoCrudRepository.findByIdCategoriaOrderByNombreAsc(idCategoria);
+	}
 
 }
