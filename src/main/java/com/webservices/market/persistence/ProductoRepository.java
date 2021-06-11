d9003bbecb7b3a1b95f8e6264b05510a0dd6454f
JhonLudenaReyes <ludena.jhon.j.6392@outlook.com>
2021-06-11 10:46:47 -0500

    Inyeccion de dependencias

--- src/main/java/com/webservices/market/persistence/ProductoRepository.java
+++ src/main/java/com/webservices/market/persistence/ProductoRepository.java
@@ -3,6 +3,7 @@ package com.webservices.market.persistence;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import com.webservices.market.domain.Product;
@@ -14,7 +15,10 @@ import com.webservices.market.persistence.mapper.ProductMapper;
 @Repository
 public class ProductoRepository implements ProductRepository{
 	
+	@Autowired
 	private ProductoCrudRepository productoCrudRepository;
+	
+	@Autowired
 	private ProductMapper mapper;
 	
 	@Override
