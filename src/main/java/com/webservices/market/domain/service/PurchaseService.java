5e90416217dd3fcd49261f174f1c0890190e8c56
JhonLudenaReyes <ludena.jhon.j.6392@outlook.com>
2021-06-18 13:32:18 -0500

    Swagger Documentation

---
+++ src/main/java/com/webservices/market/domain/service/PurchaseService.java
@@ -0,0 +1,29 @@
+package com.webservices.market.domain.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.webservices.market.domain.Purchase;
+import com.webservices.market.domain.repository.PurchaseRepository;
+
+@Service
+public class PurchaseService {
+	
+	@Autowired
+    private PurchaseRepository purchaseRepository;
+
+    public List<Purchase> getAll() {
+        return purchaseRepository.getAll();
+    }
+
+    public Optional<List<Purchase>> getByClient(String clientId) {
+        return purchaseRepository.getByClient(clientId);
+    }
+
+    public Purchase save(Purchase purchase) {
+        return purchaseRepository.save(purchase);
+    }
+}
--- src/main/java/com/webservices/market/web/controller/ProductController.java
+++ src/main/java/com/webservices/market/web/controller/ProductController.java
@@ -17,6 +17,11 @@ import org.springframework.web.bind.annotation.RestController;
 import com.webservices.market.domain.Product;
 import com.webservices.market.domain.service.ProductService;
 
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+
 @RestController
 @RequestMapping("/products")
 public class ProductController {
@@ -25,12 +30,20 @@ public class ProductController {
 	private ProductService productService;
 	
 	@GetMapping("/all")
+	@ApiOperation("Get all supermarket products")
+	@ApiResponse(code = 200, message = "OK")
 	public ResponseEntity<List<Product>> getAll(){
 		return new ResponseEntity<>(productService.getAll(), HttpStatus.OK);
 	}
 	
 	@GetMapping("/{id}")
-	public ResponseEntity<Product> getProduct(@PathVariable("id") int productId){
+	@ApiOperation("Search a product with an ID")
+	@ApiResponses({
+		@ApiResponse(code = 200, message = "Ok"),
+		@ApiResponse(code = 404, message = "Product no"),
+	})
+	public ResponseEntity<Product> getProduct(@ApiParam(value = "The id of the product", required = true, example = "7") 
+												@PathVariable("id") int productId){
 		return productService.getProduct(productId)
 				.map(product -> new ResponseEntity<>(product, HttpStatus.OK))
 				.orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
