b9e2396de3bad92b8ded20c1d144375457cc6604
JhonLudenaReyes <ludena.jhon.j.6392@outlook.com>
2021-06-19 12:52:25 -0500

    Authentication with JWT

--- src/main/java/com/webservices/market/web/security/SecurityConfig.java
+++ src/main/java/com/webservices/market/web/security/SecurityConfig.java
@@ -1,7 +1,10 @@
 package com.webservices.market.web.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -18,5 +21,21 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 		// TODO Auto-generated method stub
 		auth.userDetailsService(webserviceUserDetailsService);
 	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		// TODO Auto-generated method stub
+		http.csrf().disable().authorizeRequests().antMatchers("/**/authenticate").permitAll()
+		.anyRequest().authenticated();
+	}
+
+	@Override
+	@Bean
+	public AuthenticationManager authenticationManagerBean() throws Exception {
+		// TODO Auto-generated method stub
+		return super.authenticationManagerBean();
+	}
+	
+	
 	
 }
